1920b84e31cbaf9186c12f5b506b7e17626cbc28
Elham Mobini <elhamobini@gmail.com>
2023-05-29 13:54:04 +0330

    complete logout and destroy cookie

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -57,7 +57,15 @@ const registerUser = asyncHandler(async (req, res) => {
 // route POST /api/users/logout
 // @access public
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Logout User" });
+  // destroying cookies
+  res.cookie("jwt", "", {
+    httpOnly: true,
+
+    // expire cookie right away
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "User Logged out" });
 });
 
 // @desc Get user profile
