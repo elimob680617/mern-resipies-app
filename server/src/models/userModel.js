c004b7e9e68391ceb26c40aa34e7a9a7ec092714
Elham Mobini <elhamobini@gmail.com>
2023-05-29 13:34:03 +0330

    complete Auth User endpoint

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -21,6 +21,13 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+// is matched that password to the hashed password??
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  // returned true or false based on if the entered password matches with the hasshed password in the database
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 const User = mongoose.model("Users", userSchema);
 
 export default User;
