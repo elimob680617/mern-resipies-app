284c0eda60e452e7f986db03101544a582bd6231
Elham Mobini <elhamobini@gmail.com>
2023-05-29 14:48:51 +0330

    add auth protect routes middleware

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import connectDB from "./config/db.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import userRouter from "./routes/userRoutes.js";
+import cookieParser from "cookie-parser";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -23,6 +24,8 @@ app.get("/", (req, res) => res.send("Server is ready"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(cors());
+
+app.use(cookieParser());
 
 app.use("/api/users", userRouter);
 
---
+++ server/src/middleware/authMiddleware.js
@@ -0,0 +1,34 @@
+// to get the payload from the token which is the userId by Verify method that will verify the token and enable us to get the decoded payload information
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+
+// the middleware to protect routes >>> you have to be logged in to access that routes
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+  // get the token which I called jwt we're allowed to do this because of the cookie parser i installed
+  token = req.cookies.jwt;
+
+  // check for the token basically we check for the cookie
+  if (token) {
+    try {
+      // that decode object have userId in it
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // ultimate goal here is to set req.user to the user of the token because this req.user
+
+      // the reason the userId is there is because when we generate the token we passed in the userId ass a payload and now we get the user! And decoded will also include the password and we pass in a string of '-password'to make it the password doesn't get returned
+      req.user = await User.findById(decoded.userId).select("-password");
+
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not authorized , invalid token");
+    }
+  } else {
+    res.status(401);
+    throw new Error("Not authorized , no token");
+  }
+});
+
+export { protect };
