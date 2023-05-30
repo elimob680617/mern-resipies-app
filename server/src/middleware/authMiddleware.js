bcb7f1f15e970ad3b6e6c8ce79e334dc3254db4f
Elham Mobini <elhamobini@gmail.com>
2023-05-30 11:19:47 +0330

    complete getUserProfile Controller

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -15,9 +15,8 @@ const protect = asyncHandler(async (req, res, next) => {
       // that decode object have userId in it
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      // ultimate goal here is to set req.user to the user of the token because this req.user
-
-      // the reason the userId is there is because when we generate the token we passed in the userId ass a payload and now we get the user! And decoded will also include the password and we pass in a string of '-password'to make it the password doesn't get returned
+      /* ultimate goal here is to set req.user to the user of the token because this req.user we can access from any routes!!
+       The reason the userId is there is because when we generate the token we passed in the userId ass a payload and now we get the user! And decoded will also include the password and we pass in a string of '-password'to make it the password doesn't get returned */
       req.user = await User.findById(decoded.userId).select("-password");
 
       next();
